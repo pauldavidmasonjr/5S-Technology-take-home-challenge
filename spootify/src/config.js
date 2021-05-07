33a0a2151d3602ef1ed08ceededd94a49efe4acb
Paul Mason Jr <pauldavidmasonjr@gmail.com>
2021-05-07 13:41:54 -0500

    Update config.js

--- spootify/src/config.js
+++ spootify/src/config.js
@@ -2,7 +2,7 @@ export default {
   api: {
     baseUrl: 'https://api.spotify.com/v1',
     authUrl: 'https://accounts.spotify.com/api/token',
-    clientId: '2cd9ac8685c4442b9d05dd512c3081f7',
-    clientSecret: 'b78b35ed86544341a87c2bb13d8e0c97'
+    clientId: '',
+    clientSecret: ''
   }
 }
